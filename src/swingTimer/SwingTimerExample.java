a7570bd641cb859b4d6402c223b460d605f17b34
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-22 10:33:23 +0800

    星星向右下角运动

---
+++ src/swingTimer/SwingTimerExample.java
@@ -0,0 +1,38 @@
+package swingTimer;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Administrator on 2018/1/22.
+ */
+public class SwingTimerExample extends JFrame {
+
+	public SwingTimerExample() {
+
+		initUI();
+	}
+
+	private void initUI() {
+
+		add(new Board());
+
+		setResizable(false);
+		pack();
+
+		setTitle("Star");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame ex = new SwingTimerExample();
+				ex.setVisible(true);
+			}
+		});
+	}
+}
